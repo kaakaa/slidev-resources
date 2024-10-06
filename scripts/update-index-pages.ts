556233cb8193504d485bcfc19bdfc89630f6d92b
Yusuke Nemoto <kaakaa@users.noreply.github.com>
2024-10-06 23:47:21 +0900

    Improve scripts (#6)
    * chore: fix label
    
    * fix: introduce template engine to create index page
    
    * build: remove unnecessary line
    
    * pages: enable Google Analytics
    
    * build: enable to run manually

--- scripts/update-index-pages.ts
+++ scripts/update-index-pages.ts
@@ -1,6 +1,7 @@
 import {copyFileSync, readFileSync, writeFileSync} from 'fs';
 import {execSync} from 'child_process';
 import {globSync} from 'glob';
+import Handlebars from 'handlebars';
 
 // tsx scripts/update-index-pages.ts [gh-pages.dir]
 const args = process.argv.slice(2);
@@ -23,35 +24,30 @@ dir[dir.length - 1] === '/' ? dir : dir + '/';
 */
 
 const slidePaths = globSync(`${dir}/*/index.html`);
-const sorted = slidePaths
+const entries = slidePaths
     .map(path => {
         const elems = path.split('/');
         const name = elems[1];
         const rel = `${name}/${elems[2]}`
 
         const gitDate = execSync(`git -C ${dir} log -1 --format=%cI ${rel}`).toString().trim();
-        return { name, date: new Date(gitDate)};
+        const ret = { path, name, date: gitDate.split('T')[0], dateObj: new Date(gitDate)};
+        console.log(ret); // debug
+        return ret;
     })
-    .sort((a, b) => b.date.getTime() - a.date.getTime())
-    .map(data => {
-        const name = data.name;
-        const date = data.date.toISOString().split('T')[0];
-        return `
-        <div class="slide">
-            <a href="./${name}">
-                <img src="./${name}/preview.png" onerror="this.onerror=null; this.src='./404.png';">
-                <div>
-                    <h3>${name}</h3>
-                </div>
-            </a>
-            <div>last committed: ${date}</div>
-        </div>`
-    });
+    .sort((a, b) => b.dateObj.getTime() - a.dateObj.getTime());
 
 /* update index.html */
 let indexHtml = readFileSync('scripts/templates/tmpl_index.html', 'utf8');
-indexHtml = indexHtml.replace('{{ entries }}', sorted.join('\n'));
-writeFileSync(`${dir}/index.html`, indexHtml, 'utf-8');
+const template = Handlebars.compile(indexHtml)
+writeFileSync(
+    `${dir}/index.html`,
+    template({
+        entries,
+        'analyticsId': 'G-Q2M0L76QSW', // FIXME: parameterize
+    }),
+    'utf-8'
+);
 
 /* copy 404.png */
 copyFileSync('scripts/templates/404.png', `${dir}/404.png`);
